2065247af01c34c7d97f133d90eb954f27e567a2
inocop <inoue19851110@gmail.com>
2017-09-29 09:39:03 +0900

    add db

--- dotnet_rebuild.sh
+++ dotnet_rebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker exec dockerdotnet_web_1 systemctl stop dotnet.service
-docker exec dockerdotnet_web_1 bash -c 'cd /opt/dotnet/src/hello_dotnet && dotnet build'
+docker exec dockerdotnet_web_1 bash -c 'cd /opt/dotnet/src/hello_dotnet && dotnet restore'
+docker exec dockerdotnet_web_1 bash -c 'cd /opt/dotnet/src/hello_dotnet && dotnet build --runtime centos.7-x64'
 docker exec dockerdotnet_web_1 systemctl start dotnet.service
