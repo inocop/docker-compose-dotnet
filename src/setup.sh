d1942566cfde21218a502d2b4429f08635d7436b
inocop <inoue19851110@gmail.com>
2017-10-16 10:16:59 +0900

    add xunit project

--- src/setup.sh
+++ src/setup.sh
@@ -15,9 +15,23 @@ sed -i -e "s/DataSource=app.db/server=db;userid=testing;password=testing;databas
 dotnet add package Pomelo.EntityFrameworkCore.MySql --version 2.0.0
 sed -i -e "s/options.UseSqlite/options.UseMySql/g" ./Startup.cs
 
+# xunit
+dotnet add package dotnet-xunit --version 2.3.0
+
 # build & start
 dotnet publish --runtime centos.7-x64 --output bin/publish
 systemctl restart dotnet.service
 
 # db migrate
 dotnet ef database update
+
+
+# create xunit project
+cd /opt/dotnet/src
+dotnet new xUnit --output hello_dotnet.test --framework netcoreapp2.0
+dotnet add hello_dotnet.test/hello_dotnet.test.csproj reference hello_dotnet/hello_dotnet.csproj
+
+
+# create sln
+dotnet new sln --name hello_dotnet
+dotnet sln add **/*.csproj
